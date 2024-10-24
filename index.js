bf5394b8e96fbb3f16d50e8373d7078364640438
Splatform2000 <sahasraplatform@gmail.com>
2024-10-24 15:39:02 +0530

    db port changes

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.json({ limit: '10mb' })); // JSON body parser with limit
 app.use(cors()); // Enable CORS
 
 // Connect to MongoDB with the direct connection string
-const mongoURI = "mongodb+srv://school:school@school.vjfuq.mongodb.net/school?retryWrites=true&w=majority&appName=school";
+//const mongoURI = "mongodb+srv://school:school@school.vjfuq.mongodb.net/school?retryWrites=true&w=majority&appName=school";
+const mongoURI ="mongodb+srv://splatform:Splatform2000@splatform.rohck.mongodb.net/?retryWrites=true&w=majority&appName=Splatform"
 mongoose
     .connect(mongoURI, {
         useNewUrlParser: true,
